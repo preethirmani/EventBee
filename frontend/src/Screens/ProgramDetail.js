217d26e24cb0af0f44bc8a31265655f029394d30
Preethi <writetopreethir@gmail.com>
2022-11-29 01:02:30 -0800

    Program.js Implemented

--- frontend/src/Screens/ProgramDetail.js
+++ frontend/src/Screens/ProgramDetail.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
+import {Card} from 'react-bootstrap'
 import progrmas from '../programs';
+import programs from '../programs';
 
 const ProgramDetail = () => {
   const params = useParams();
-  const progrm = progrmas.find(p => p._id === params.id);
-  console.log('params.id'+params.id);
-  console.log(`Program:: ${JSON.stringify(progrm)}`);
+  const progrm = progrmas.find(p => 
+   
+    p._id == params.id
+  );
+  console.log(progrm.name);
+ 
   return (
-    <div>ProgramDetail</div>
+    <>
+    <Card >
+      <Card.Img src={progrm.image} variant='top'></Card.Img>
+    </Card>
+    </>
+
+  
   )
 }
 
--- frontend/src/programs.js
+++ frontend/src/programs.js
@@ -76,21 +76,23 @@ const programs = [
 
   {
    _id: 5,
-    name:'Taichi',
-    image: '/images/taichi.jpg',
-    description:'Begin your Taichi journey: Traditional Movements for Health/Rejuvenation. ',
-    host:'Quality of Life',
-    date:'Sun, Nov 27, 2022',
-    time:'11:00 AM EST',
+    name:'Mindfulness, Music & Deep Listening',
+    image: '/images/meditation.jpg',
+    description:`Guided meditations in stillness and movement offer space to slow down, rest, revive and become fully present. Deep Listening practices by Pauline Oliveros and others helps us ground ourselves in our listening body and in our connection with the Earth. We sound and create together, using our voices and instruments you bring in ways that feel natural and spontaneous.
+
+      You are invited to engage as much or as little as your energy allows - resting and listening are always options, too. `,
+    host:'Susanne Olbrich and Joe Holtaway',
+    date:'Once in a month starting 19 Dec',
+    time:'9:00 PM PST',
     location:'Online',
-    price:'Free',
-    rating:'4.5',
-    numReviews:'170',
+    price:'45',
+    rating:'4.7',
+    numReviews:'115',
     seatsAvailable:'',
     category:'Health',
     requirements:'',
     about:'',
-    duration:'30 minutes'
+    duration:'60 minutes'
   },
 
   {
